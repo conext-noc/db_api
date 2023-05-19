5c42494b47776f40fcac4a5882d25cd072240aff
Ricardo Vera <ricardovera71@gmail.com>
2023-05-19 04:28:47 -0500

    fix(url)

--- db_api/urls.py
+++ db_api/urls.py
@@ -3,6 +3,6 @@ from .views import DbApi, SignIn, AddUser
 
 urlpatterns = [
     path("", DbApi.as_view()),
-    path("/signin", SignIn.as_view()),
-    path("/adduser", AddUser.as_view()),
+    path("signin", SignIn.as_view()),
+    path("adduser", AddUser.as_view()),
 ]
