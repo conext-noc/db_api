8d9d5967266c0f35e3d7bf15a7f1416291b4c3de
Ricardo Vera <ricardovera71@gmail.com>
2023-08-08 16:35:10 -0500

    fix(ms-health-check)

--- db_api/ms_health_status.py
+++ db_api/ms_health_status.py
@@ -1,15 +1,13 @@
 import os
-from dotenv import load_dotenv
 import boto3
+from dotenv import load_dotenv
 from botocore.exceptions import NoCredentialsError
 
 load_dotenv()
 
 
 def get_health_status():
-    data = {"Ops-env": {}, "Ins-env": {}, "Sch-env": {}, "Mod-env": {}, "Mon-env": {}}
     res = []
-    envs = list(data.keys())
     try:
         # Create a session using your AWS access key and secret access key
         session = boto3.Session(
@@ -17,26 +15,22 @@ def get_health_status():
             aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"],
         )
 
-        # Create an AWS Elastic Beanstalk client
         eb_client = session.client("elasticbeanstalk", region_name="us-west-2")
 
-        # Get information about the environment
-        for environment in envs:
-            eb_envs = eb_client.describe_environments(EnvironmentNames=[environment])
-            if len(eb_envs["Environments"]) == 0:
-                data[environment] = None
-            else:
-                response = eb_envs["Environments"][0]
-                data[environment]["health"] = (
-                    response["HealthStatus"]
-                    if "HealthStatus" in response
-                    else response["Health"]
-                )
-                data[environment]["application"] = response["ApplicationName"]
-                data[environment]["version"] = response["VersionLabel"]
-                data[environment]["status"] = response["Status"]
-        for environments in data.items():
-            res.append(environments[1])
+        envs = eb_client.describe_environments()["Environments"]
+
+        for env in envs:
+            res.append(
+                {
+                    "name": env["EnvironmentName"],
+                    "app-name": env["ApplicationName"],
+                    "version": env.get("VersionLabel") or "",
+                    "url-eb": env["CNAME"],
+                    "status": env["Status"],
+                    "health": env["Health"],
+                    "health-status": env["HealthStatus"],
+                }
+            )
 
         return {"error": False, "message": "success", "data": res}
 
@@ -46,3 +40,6 @@ def get_health_status():
             "message": "Unable to locate AWS credentials.",
             "data": None,
         }
+
+
+get_health_status()
