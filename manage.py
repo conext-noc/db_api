acf3895f42887c86e482926521af7eb65b63d632
Ricardo Vera <ricardovera71@gmail.com>
2023-12-03 15:11:36 -0600

    fix(port)

--- manage.py
+++ manage.py
@@ -10,8 +10,8 @@ def main():
     try:
         from django.core.management import execute_from_command_line
 
-        from django.core.management.commands.runserver import Command as runserver
-        runserver.default_port = "7000"
+        # from django.core.management.commands.runserver import Command as runserver
+        # runserver.default_port = "7000"
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
